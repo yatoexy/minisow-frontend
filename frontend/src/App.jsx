e30c22b6deff8a6816ff63255a5911cda908654a
Yatharth Tiwari <tyatharth17@gmail.com>
2025-11-20 17:01:00 +0530

    Fixed login UI mobile/tablet padding issue + updated navbar + responsive improvements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
@@ -7,18 +6,18 @@ import ForgotPostal from "./pages/forgot/ForgotPostal";
 import ForgotSuccess from "./pages/forgot/ForgotSuccess";
 import ForgotError from "./pages/forgot/ForgotError";
 
-// LOGIN PAGE (renamed)
+// LOGIN PAGE
 import LoginPage from "./pages/login/LoginDesktop";
 
 // REGISTER PAGE
 import Register from "./pages/Register";
 
-// PRICELIST PAGE (after login)
+// PRICELIST PAGE
 import PriceList from "./pages/PriceList";
 
 // STATIC PAGES
 import Home from "./pages/Home";
-import Order from "./pages/order";
+import Order from "./pages/order";    // ✔ lowercase file support
 import OurCustomers from "./pages/OurCustomers";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
@@ -32,32 +31,22 @@ export default function App() {
     <Router>
       <Routes>
 
-        {/* LOGIN (DEFAULT PAGE) */}
-        <Route 
-          path="/" 
-          element={<LoginPage setLoggedIn={setLoggedIn} />} 
-        />
+        <Route path="/" element={<LoginPage setLoggedIn={setLoggedIn} />} />
 
-        {/* REGISTER PAGE */}
         <Route path="/register" element={<Register />} />
 
-        {/* PRICELIST PAGE */}
         <Route
           path="/pricelist"
-          element={
-            loggedIn ? <PriceList /> : <LoginPage setLoggedIn={setLoggedIn} />
-          }
+          element={loggedIn ? <PriceList /> : <LoginPage setLoggedIn={setLoggedIn} />}
         />
 
-        {/* FORGOT PAGES */}
         <Route path="/forgot" element={<ForgotEmail />} />
         <Route path="/forgot-postal" element={<ForgotPostal />} />
         <Route path="/forgot-success" element={<ForgotSuccess />} />
         <Route path="/forgot-error" element={<ForgotError />} />
 
-        {/* STATIC WEBSITE PAGES */}
         <Route path="/home" element={<Home />} />
-        <Route path="/order" element={<Order />} />
+        <Route path="/order" element={<Order />} />   {/* ✔ fixed */}
         <Route path="/customers" element={<OurCustomers />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<ContactUs />} />
